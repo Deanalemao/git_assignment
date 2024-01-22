d0d0e1964817aee21c1e80196b8d06d8c47edebe
Dean Alemao <deanalemao21@gmail.com>
2024-01-22 20:52:31 +0530

    modified program

---
+++ src/Demo/sub.java
@@ -0,0 +1,22 @@
+package Demo;
+
+import java.util.Scanner;
+
+public class sub {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		int a,b,sub=0;
+		
+	System.out.println("Enter First number:");
+	a=scan.nextInt();
+	System.out.println("Enter second number:");
+	b=scan.nextInt();
+	
+	sub=a-b;
+	System.out.println("Difference is:"+sub);
+				
+
+}
+}
+
